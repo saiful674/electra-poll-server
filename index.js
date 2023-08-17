f2d1b47410615de448ae2f1920d5701fc6d9bbd4
maufdev09 <marufdev09@gmail.com>
2023-08-17 17:25:47 +0600

    mongodb user

--- index.js
+++ index.js
@@ -30,8 +30,23 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db("electrapoll");
+    const userCollection = database.collection("users");
 
 
+
+
+   // Users
+   app.post("/users", async (req, res) => {
+    const user = req.body;
+    const query = { email: user.email };
+    const existingUser = await userCollection.findOne(query);
+    if (existingUser) {
+      return res.send({ message: "account already exist" });
+    }
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  });
 
 
 
