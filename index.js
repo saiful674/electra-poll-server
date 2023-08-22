2882c7143309c0429d7c8a3de3bc6b304ccee87d
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-08-22 21:12:18 +0600

    email sending logic update -mahmud

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ async function run() {
           _id: new ObjectId(id),
         });
 
-        const emailHtml = `
+        let emailHtml = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -203,8 +203,8 @@ async function run() {
                     </td>
                 </tr>
                 <tr>
-                    <td bgcolor="#f0fdf4" style="padding: 40px 20px; color: black;line-height:50px">
-                        <h3>You are cordially invited to cast your vote in the upcoming ${getElection?.organization} - ${getElection?.title} election.</h3>
+                    <td bgcolor="#f0fdf4" style="padding: 40px 20px; color: black;line-height:20px">
+                        <h3>You are cordially invited to cast your vote in the upcoming ${getElection?.title} election - ${getElection?.organization}.</h3>
                         <p>Hello,</p>
                         <p style="color: black">We are employing a sophisticated online voting system to ensure accuracy and transparency. You have been allocated a unique voting key, granting you one-time access to this process. Please treat this key with confidentiality and avoid sharing or forwarding this communication.</p>
                         <p>Should you have any queries or wish to share feedback regarding the election, or if you prefer not to receive subsequent voting notifications, please contact ${getElection?.email}</p>
@@ -226,17 +226,77 @@ async function run() {
 
         getElection.voterEmails?.map((e) => emails.push(e.email));
 
-        try {
-          const mailInfo = await transporter.sendMail({
-            from: "codecrafters80@gmail.com",
-            to: emails,
-            subject: "Invitation for Your Opinion",
-            html: emailHtml,
-          });
+        for (const voter of getElection.voterEmails) {
+          try {
+            const mailInfo = await transporter.sendMail({
+              from: "codecrafters80@gmail.com",
+              to: voter.email,
+              subject: `Vote Now: ${getElection?.title}`,
+              html: `
+              <!DOCTYPE html>
+              <html lang="en">
+              <head>
+                  <meta charset="UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <title>Email Template</title>
+                  <style>
+                      body {
+                          font-family: Arial, sans-serif;
+                          margin: 0;
+                          padding: 0;
+                          border-radius: 15px;
+                      }
+          
+                      @media only screen and (max-width: 576px) {
+                          body {
+                              width: 100% !important;
+                          }
+                      }
+          
+                      @media only screen and (max-width: 376px) {
+                          body {
+                              width: 100% !important;
+                          }
+                      }
+                  </style>
+              </head>
+              <body style="margin: 0 auto;">
+                  <table role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0">
+                      <tr>
+                          <td align="center" style="padding: 20px 0;">
+                              <img src="https://i.ibb.co/J2k86ts/logo.png" alt="Company Logo" width="150">
+                          </td>
+                      </tr>
+                      <tr>
+                          <td bgcolor="#f0fdf4" style="padding: 40px 20px; color: black;line-height:20px">
+                              <h3>You are cordially invited to cast your vote in the upcoming ${getElection?.title} election - ${getElection?.organization}.</h3>
+                              <p>Hello,</p>
+                              <p style="color: black">We are employing a sophisticated online voting system to ensure accuracy and transparency. You have been allocated a unique voting key, granting you one-time access to this process. Please treat this key with confidentiality and avoid sharing or forwarding this communication.</p>
+                              <p>Should you have any queries or wish to share feedback regarding the election, or if you prefer not to receive subsequent voting notifications, please contact ${getElection?.email}</p>
 
-          console.log("Message sent: %s", mailInfo.messageId);
-        } catch (error) {
-          console.error("Error sending email:", error);
+                              <p style="padding-bottom: 10px">you will need to enter the access key and password to vote. Don't share it with anybody</p>
+                              <p>Access Key: ${voter.accessKey}</p>
+                              <p>Password: ${voter.password}</p>
+                              <hr />
+          
+                              <p>Thank you for your participation.</p>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td bgcolor="#f4f4f4" style="text-align: center; padding: 10px 0;">
+                              <p>&copy; 2023 Electro Poll. All rights reserved.</p>
+                          </td>
+                      </tr>
+                  </table>
+              </body>
+              </html>
+          `
+            });
+
+            console.log("Message sent: %s", mailInfo.messageId);
+          } catch (error) {
+            console.error("Error sending email:", error);
+          }
         }
       }
       res.send(result);
