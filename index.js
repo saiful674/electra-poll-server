58c9725590122e3f40c60fa8b203777659904d43
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-09-05 22:21:13 -0700

    notification get by date

--- index.js
+++ index.js
@@ -528,7 +528,8 @@ async function run() {
     app.get("/notifications/:email", async (req, res) => {
       const email = req.params.email;
       const query = { userEmail: email };
-      const result = await notificationCollection.find(query).toArray();
+      
+      const result = await notificationCollection.find(query).sort({timestamp: -1}).toArray();
 
       res.send(result);
     });
